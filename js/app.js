276bff6a819bf36513b52648f2e990d773edb70f
Neil DCruz <neildcruz@Neils-MBP.home>
2015-07-11 15:56:19 -0700

    update site

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 	$("a.scroll[href^='#']").on('click', function(e) {
 		e.preventDefault();
 		var hash = this.hash;
-		$('html, body').animate({ scrollTop: $(this.hash).offset().top}, 1000, function(){window.location.hash = hash;});
+		$('html, body').animate({ scrollTop: $(this.hash).offset().top}, 1000	, function(){window.location.hash = hash;});
+
+		$(".navbar-collapse").collapse('hide');
 	});
 
 	$("section").css("min-height", $(window).height());
